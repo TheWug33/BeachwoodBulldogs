2ae9b3f3c7b76ed10ca2bdb786fcbb5daf774df2
TheWug33 <scott.wbeverly@gmail.com>
2026-08-19 08:32:05 -0400

    Add files via upload

--- bulldogs-site/src/lib/sheets.js
+++ bulldogs-site/src/lib/sheets.js
@@ -14,13 +14,13 @@
 import Papa from 'papaparse'
 
 export const SHEET_URLS = {
-  roster: '',
-  schedule: '',
-  standings: '',
-  coaches: '',
-  photos: '',
-  videos: '',
-  info: '',
+  roster: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=266633233&single=true&output=csv',
+  schedule: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=1906911618&single=true&output=csv',
+  standings: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=2141598853&single=true&output=csv',
+  coaches: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=1903247065&single=true&output=csv',
+  photos: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=156056436&single=true&output=csv',
+  videos: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=2136287562&single=true&output=csv',
+  info: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5HgGPQ0MSBo02p1N7UnKnVQQG53C5g-9og0lJh5F8IUtpoz3SIDeYz1Kt8-BeLg/pub?gid=967153679&single=true&output=csv',
 }
 
 export async function fetchSheet(url) {
